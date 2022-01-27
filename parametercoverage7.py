8946e9391e0931a548af81ee1069c718afcb3a2d
sajjavenkey50 <48043757+sajjavenkey50@users.noreply.github.com>
2022-01-27 13:56:31 +0530

    Create parametercoverage7.py

---
+++ parametercoverage7.py
@@ -0,0 +1,20 @@
+# import pandas lib as pd
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+
+
+dfTextFile = pd.read_fwf('Psm_coverage_psm_dev.txt', memory_map=True, header=None)
+#print(dfTextFile.info())
+for col in dfTextFile:
+  
+  
+print('#Step 7 -- Function to find the average of parameters')
+print(dfTextFile.describe())
