dc192f829db72bfa09ce9193308efcdafcab0d5c
sajjavenkey50 <48043757+sajjavenkey50@users.noreply.github.com>
2022-01-27 14:05:25 +0530

    Update parametercoverage7.py

--- parametercoverage7.py
+++ parametercoverage7.py
@@ -14,7 +14,7 @@ from email import encoders
 dfTextFile = pd.read_fwf('Psm_coverage_psm_dev.txt', memory_map=True, header=None)
 #print(dfTextFile.info())
 for col in dfTextFile:
-   print(dfTextFile[col].unique())
+   #print(dfTextFile[col].unique())
   
   
 print('#Step 7 -- Function to find the average of parameters')
