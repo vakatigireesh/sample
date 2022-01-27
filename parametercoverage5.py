80fde4df82f7619672de266af03d8f6f3254db51
sajjavenkey50 <48043757+sajjavenkey50@users.noreply.github.com>
2022-01-27 11:00:41 +0530

    Create parametercoverage5.py

---
+++ parametercoverage5.py
@@ -0,0 +1,29 @@
+# import pandas lib as pd
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+# Step 5 -- Function to develop a report and send an email 
+print('Function to develop a report and send an email')
+print(buildStatus)
+msg = MIMEMultipart()
+msg['From'] = "giri.vakati@gmail.com"
+msg['To'] = "venkateswarluec@gmail.com" 
+msg['Subject'] = "Build status" 
+body = """Hi Team, Please find the build status 
+Build is {}"""
+msg.attach(MIMEText(body, 'plain'))
+s = smtplib.SMTP('smtp.gmail.com', 587) 
+s.starttls() # for security
+s.login("giri.vakati@gmail.com", "atrtxgutxyaxnuxv") 
+text = msg.as_string() 
+# sending the mail 
+s.sendmail("giri.vakati@gmail.com", "venkateswarluec@gmail.com" , text.format(buildStatus))
+s.quit()
+print('EMAIL sent')
