de3e753540a6767e148d59faa342231befb400c9
sajjavenkey50 <48043757+sajjavenkey50@users.noreply.github.com>
2022-01-27 11:05:52 +0530

    Update parametercoverage5.py

--- parametercoverage5.py
+++ parametercoverage5.py
@@ -9,6 +9,33 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 
+
+def convertXMLtoDataFrame():
+    print("convertXMLtoDataFrame")
+    prstree = ET.parse('config.xml')
+    root = prstree.getroot()
+
+    store_items = []
+    all_items = []
+    for storeno in root.find('POOL').findall('Parameter'):  
+        obj=storeno.attrib.get('obj')
+        no= storeno.find('No').attrib.get('Nsp')
+        res= storeno.find('Responsible').text
+        store_items = [obj,no,res]
+        all_items.append(store_items)
+  
+    xmlToDf = pd.DataFrame(all_items,columns=['Object','Number','Responsible'])        
+    return xmlToDf;
+
+xmlToDf= convertXMLtoDataFrame()
+
+
+# Step 4-- Function to pass or fail the build depending on the previous step 
+
+print('Function to pass or fail the build depending on the previous step')
+def findParameterFromXMLDataFrame(xmlToDf, searchString):
+    return xmlToDf[xmlToDf['Object'].str.contains(searchString)]
+    
 searchString='RTDB_GP_ESWITCH_OUTPUTS_E1'
 resultDataFrame=findParameterFromXMLDataFrame(xmlToDf,searchString)
 print(resultDataFrame.size)
@@ -19,6 +46,7 @@ if resultDataFrame.size>0:
 else:
     buildStatus='FAIL'
     print('FAIL')
+
 
 
 # Step 5 -- Function to develop a report and send an email 
